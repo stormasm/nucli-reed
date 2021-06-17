56c7b510c5bb403a104415951f1753af0cafa4ad
Michael Angerman <stormasm@gmail.com>
2021-06-17 11:15:38 -0700

    repl-result is up and running with the std result being returned instead of the crossterm result

---
+++ examples/repl-result.rs
@@ -0,0 +1,24 @@
+use reedline::{DefaultPrompt, Reedline, Signal, DEFAULT_PROMPT_COLOR, DEFAULT_PROMPT_INDICATOR};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut line_editor = Reedline::new();
+    let prompt = DefaultPrompt::new(DEFAULT_PROMPT_COLOR, DEFAULT_PROMPT_INDICATOR, 1);
+
+    loop {
+        let sig = line_editor.read_line(&prompt)?;
+        match sig {
+            Signal::CtrlD | Signal::CtrlC => {
+                line_editor.print_crlf().unwrap();
+                break;
+            }
+            Signal::Success(buffer) => {
+                println!("We processed: {}", buffer);
+            }
+            Signal::CtrlL => {
+                line_editor.clear_screen().unwrap();
+            }
+        }
+    }
+    Ok(())
+}
