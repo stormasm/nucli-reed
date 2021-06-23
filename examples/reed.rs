8beddb181aabaaba80beec0e3f380f360846f07f
Michael Angerman <stormasm@gmail.com>
2021-06-23 06:29:51 -0700

    make mod app public and switch Options::new to CliOptions::new()

--- examples/reed.rs
+++ examples/reed.rs
@@ -1,8 +1,8 @@
-use nu_cli_reed::{create_default_context, Options};
+use nu_cli_reed::{app::CliOptions, create_default_context};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let options = Options::new();
+    let options = CliOptions::new();
     let context = create_default_context(true)?;
     nu_cli_reed::cli(context, options)?;
     Ok(())
