35889b91a0a23a8235100aad1362d94942d102fc
Michael Angerman <stormasm@gmail.com>
2021-06-23 06:33:15 -0700

    now that we have migrated all of our code to 0.33.0 we can delete the legacy crates nu-cli-rusty and nu-cli which were there to show backwards compatability for 0.32.0 and they are no longer needed

--- crates/nu-cli-rusty/src/shell.rs
+++
@@ -1,9 +0,0 @@
-#![allow(clippy::module_inception)]
-
-#[cfg(feature = "rustyline-support")]
-pub(crate) mod completer;
-#[cfg(feature = "rustyline-support")]
-pub(crate) mod helper;
-
-#[cfg(feature = "rustyline-support")]
-pub(crate) use helper::Helper;
