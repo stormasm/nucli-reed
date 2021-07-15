d849b3f8c25c69ab5a6ad59fe2f4d9bb2d4c22a7
Michael Angerman <stormasm@gmail.com>
2021-07-14 18:38:27 -0700

    delete prelude.rs which is no longer used, clean up lib.rs, delete assets which are no longer needed, update Cargo.toml to match 0.34.0 version of nu-cli

--- crates/nu-cli-reed/src/lib.rs
+++ crates/nu-cli-reed/src/lib.rs
@@ -1,9 +1,3 @@
-#[cfg(test)]
-extern crate quickcheck;
-#[cfg(test)]
-#[macro_use(quickcheck)]
-extern crate quickcheck_macros;
-
 pub mod app;
 mod cli;
 pub use crate::cli::cli;
@@ -11,16 +5,4 @@ pub use crate::cli::cli;
 pub use crate::app::App;
 pub use crate::cli::{parse_and_eval, register_plugins, run_script_file};
 
-pub use nu_command::{
-    commands::NuSignature as Nu, commands::Version as NuVersion, create_default_context,
-};
-pub use nu_data::config;
-pub use nu_data::dict::TaggedListBuilder;
-pub use nu_data::primitive;
-pub use nu_data::value;
-pub use nu_stream::{ActionStream, InputStream, InterruptibleStream};
-pub use nu_value_ext::ValueExt;
-pub use num_traits::cast::ToPrimitive;
-
-// TODO: Temporary redirect
-pub use nu_protocol::{did_you_mean, TaggedDictBuilder};
+pub use nu_command::create_default_context;
