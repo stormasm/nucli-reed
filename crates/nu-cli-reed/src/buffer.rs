0baf1c6aa0848a718dd8d17e1f34619c09496ea0
Michael Angerman <stormasm@gmail.com>
2021-07-27 21:54:30 -0700

    add call to buffer for complete starting to work

--- crates/nu-cli-reed/src/buffer.rs
+++ crates/nu-cli-reed/src/buffer.rs
@@ -1,3 +1,5 @@
+use crate::shell::{Completer, CompletionContext, Helper};
+
 use nu_engine::evaluation_context::EvaluationContext;
 use nu_engine::script::LineResult;
 
@@ -13,7 +15,7 @@ fn chomp_newline(s: &str) -> &str {
 /// input and output parameters
 pub fn process_buffer(
     script_text: &str,
-    _ctx: &EvaluationContext,
+    ctx: &EvaluationContext,
     _redirect_stdin: bool,
     _span_offset: usize,
     _cli_mode: bool,
@@ -23,6 +25,17 @@ pub fn process_buffer(
     } else {
         let line = chomp_newline(script_text);
         println!("{}", line);
+
+        let helper = Helper::new(EvaluationContext::basic());
+        let ctx = CompletionContext(ctx);
+        helper.complete(line, 0, &ctx);
+
+        /*
+                let ctx = CompletionContext(&self.context);
+                let (position, suggestions) = self.completer.complete(line, pos, &ctx);
+                let suggestions = suggestions.into_iter().map(CompletionSuggestion).collect();
+        */
+
         LineResult::Success(line.to_string())
     }
 }
